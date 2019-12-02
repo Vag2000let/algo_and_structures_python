1101f134fcab2c60326993fde6d39b86edadee44
Vagan <vagan2000let@mail.ru>
2019-12-02 23:15:04 +0300

    Done HW_5

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,34 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+from collections import namedtuple
+
+
+ASC = int(input("Введите кол-во предприятий: "))
+COMPANIES = namedtuple("company", "name quarter_1 quarter_2 quarter_3 quarter_4")
+PROFIT_AVER = {}
+
+for i in range(ASC):
+    Company = COMPANIES(name=input("Введите название предприятия: "),
+                        quarter_1=int(input("Введите прибыль за первый квартал: ")),
+                        quarter_2=int(input("Введите прибыль за второй квартал: ")),
+                        quarter_3=int(input("Введите прибыль за третий квартал: ")),
+                        quarter_4=int(input("Введите прибыль за четвертый квартал: ")))
+    PROFIT_AVER[Company.name] = (Company.quarter_1 + Company.quarter_2 + Company.quarter_3 + Company.quarter_4)
+
+print(COMPANIES)
+
+TOTAL_AVER = 0
+for value in PROFIT_AVER.values():
+    TOTAL_AVER += value
+TOTAL_AVER = TOTAL_AVER / ASC
+
+
+for key, value in PROFIT_AVER.items():
+    if value > TOTAL_AVER:
+        print(key, " - прибыль выше среднего")
+    if value < TOTAL_AVER:
+        print(key, " - прибыль ниже среднего")
+    if value == TOTAL_AVER:
+        print(key, " - средняя прибыль")
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,28 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
+from collections import deque
+
+num_a = deque(input("Введите первое число: "))
+num_b = deque(input("Введите второе число: "))
+
+
+def hex_sum(num_a, num_b):
+    num_a = "".join([i for i in num_a])
+    num_b = "".join([i for i in num_b])
+    sum = hex((int(float.fromhex(num_a) + float.fromhex(num_b))))
+    sum = deque(sum[2::].upper())
+    print("Сумма: ", sum)
+
+
+def hex_mul(num_a, num_b):
+    num_a = "".join([i for i in num_a])
+    num_b = "".join([i for i in num_b])
+    sum = hex((int(float.fromhex(num_a) * float.fromhex(num_b))))
+    sum = deque(sum[2::].upper())
+    print("Произведение: ", sum)
+
+
+hex_sum(num_a, num_b)
+# hex_mul(num_a, num_b)
