8c35eaba7c0fc5bfbb8bc1388bf7d8fcb6de9a23
Vagan <vagan2000let@mail.ru>
2019-12-08 23:48:21 +0300

    Done HW_7

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,43 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
+
+import random
+import timeit
+
+
+# Стандартный подход
+def bubble_sort(LIST):
+    w = 1
+
+    while w < len(LIST):
+        for i in range(len(LIST) - w):
+            if LIST[i] < LIST[i + 1]:
+                LIST[i], LIST[i + 1] = LIST[i + 1], LIST[i]
+        w += 1
+    return LIST
+
+# Дорабртанный
+# Если за проход не совершается ни одной сортировки то прекращение
+
+def bubble_sort_upgrade(LIST):
+    q = 1
+    no_sort = 0
+
+    while q < len(LIST):
+        for i in range(len(LIST) - q):
+            if LIST[i] < LIST[i + 1]:
+                LIST[i], LIST[i + 1] = LIST[i + 1], LIST[i]
+                no_sort = 1
+        if no_sort == 0:
+            break
+        q += 1
+    return LIST
+
+
+LIST = [random.randint(-100, 100) for i in range(10)]
+print(bubble_sort(LIST))
+print(bubble_sort_upgrade(LIST))
+
+print(timeit.timeit("bubble_sort(LIST)", setup="from __main__ import bubble_sort, LIST"))
+print(timeit.timeit("bubble_sort_upgrade(LIST)", setup="from __main__ import bubble_sort_upgrade, LIST"))
--- Lesson_7/2.py
+++ Lesson_7/2.py
@@ -3,3 +3,46 @@
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
 """
+
+import random
+
+
+def merge_and_sort(LIST):
+    if len(LIST) > 1:
+        cen = len(LIST) // 2
+        left = LIST[:cen]
+        rigth = LIST[cen:]
+
+        merge_and_sort(left)
+        merge_and_sort(rigth)
+
+        q = 0
+        w = 0
+        e = 0
+
+        while q < len(left) and w < len(rigth):
+            if left[q] < rigth[w]:
+                LIST[e] = left[q]
+                q += 1
+            else:
+                LIST[e] = rigth[w]
+                w += 1
+            e += 1
+
+        while q < len(left):
+            LIST[e] = left[q]
+            q += 1
+            e += 1
+
+        while w < len(rigth):
+            LIST[e] = rigth[w]
+            w += 1
+            e += 1
+
+
+NUMBER = int(input("Введите число элементов: "))
+LIST = [random.random()*50 for i in range(NUMBER)]
+
+print(LIST)
+merge_and_sort(LIST)
+print(LIST)
